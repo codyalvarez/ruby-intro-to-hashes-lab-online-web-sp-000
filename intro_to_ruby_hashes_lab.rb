e6c3a9aa6ae86dd2a13b5c936f8370de8c87a650
Cody Alvarez <cody_alvarez19@outlook.com>
2021-03-09 02:18:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,12 +18,12 @@ end
 # 			:pieces => 4,
 # 			:names => {}
 # 			:rent_in_dollars = {}
-# 
+#
 # 		}
 # 	}
-# 
+#
 # 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-# 
+#
 # end
 
 def monopoly_with_third_tier
